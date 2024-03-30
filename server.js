76a76c92fd6990a2bad29d0e7946e21a4d4ddbe1
Tobi Ogunsola <tomobi552@gmail.com>
2024-03-30 16:10:42 +0100

    node

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.end('<h1>Hello Node!!!!</h1>\n');
+}).listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
